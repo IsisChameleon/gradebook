a31aed9015e01ac88719f8ce9c04e24a12e39155
IsisChameleon <12711084+IsisChameleon@users.noreply.github.com>
2019-12-10 11:17:50 +1100

    End of Chapter 3 on Basics

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;  // Console is inside System namepspace 
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var numbers = new double[3];
+            // // numbers[0] = 12.7;
+            // // numbers[1] = 10.3;
+            // // numbers[2] = 6.11;
+
+            var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+            // List<double> grades = new List<double>();
+            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+            // List provides a dynamically sized collection of items
+            grades.Add(56.1);
+
+            double result = 0;
+            foreach (var i in numbers)
+            {
+                result+=i;
+            }
+            Console.WriteLine($"result : {result}");
+
+            double result2 = 0;
+            foreach (var i in grades)
+            {
+                result2+=i;
+            }
+            Console.WriteLine($"result2 : {result2:N1}");  
+
+            double average = grades.Count > 0 ? grades.Average() : double.NaN;  
+
+            Console.WriteLine($"Avergae results2 : {average}"); 
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("hello bobo");
+            }
+        }
+    }
+}
